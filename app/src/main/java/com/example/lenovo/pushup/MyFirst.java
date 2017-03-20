956887ce252b044f412fa4dc55e052e1b4d917b4
shady elabady <shady elabady>
2017-03-20 12:44:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/pushup/MyFirst.java
@@ -0,0 +1,19 @@
+package com.example.lenovo.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFirst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_first);
+    }
+
+    private void TestUpload{} {
+
+        int i = 1+1;
+
+    }
+}
